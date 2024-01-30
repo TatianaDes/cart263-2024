180aa605873df47d67f7e8f4e7bda67d43d51ecd
TatianaDes <tatiana.d0714@gmail.com>
2024-01-30 18:47:58 -0500

    MP1: Testing out why my microphone is not working

--- mini-projects/voices-jam/js/script.js
+++ mini-projects/voices-jam/js/script.js
@@ -7,266 +7,88 @@ This is a portotype of a program about the importance of being kind towards all 
 
 "use strict";
 
-
-const animals = [
-    "aardvark",
-    "alligator",
-    "alpaca",
-    "antelope",
-    "ape",
-    "armadillo",
-    "baboon",
-    "badger",
-    "bat",
-    "bear",
-    "beaver",
-    "bison",
-    "boar",
-    "buffalo",
-    "bull",
-    "camel",
-    "canary",
-    "capybara",
-    "cat",
-    "chameleon",
-    "cheetah",
-    "chimpanzee",
-    "chinchilla",
-    "chipmunk",
-    "cougar",
-    "cow",
-    "coyote",
-    "crocodile",
-    "crow",
-    "deer",
-    "dingo",
-    "dog",
-    "donkey",
-    "dromedary",
-    "elephant",
-    "elk",
-    "ewe",
-    "ferret",
-    "finch",
-    "fish",
-    "fox",
-    "frog",
-    "gazelle",
-    "gila monster",
-    "giraffe",
-    "gnu",
-    "goat",
-    "gopher",
-    "gorilla",
-    "grizzly bear",
-    "ground hog",
-    "guinea pig",
-    "hamster",
-    "hedgehog",
-    "hippopotamus",
-    "hog",
-    "horse",
-    "hyena",
-    "ibex",
-    "iguana",
-    "impala",
-    "jackal",
-    "jaguar",
-    "kangaroo",
-    "koala",
-    "lamb",
-    "lemur",
-    "leopard",
-    "lion",
-    "lizard",
-    "llama",
-    "lynx",
-    "mandrill",
-    "marmoset",
-    "mink",
-    "mole",
-    "mongoose",
-    "monkey",
-    "moose",
-    "mountain goat",
-    "mouse",
-    "mule",
-    "muskrat",
-    "mustang",
-    "mynah bird",
-    "newt",
-    "ocelot",
-    "opossum",
-    "orangutan",
-    "oryx",
-    "otter",
-    "ox",
-    "panda",
-    "panther",
-    "parakeet",
-    "parrot",
-    "pig",
-    "platypus",
-    "polar bear",
-    "porcupine",
-    "porpoise",
-    "prairie dog",
-    "puma",
-    "rabbit",
-    "raccoon",
-    "ram",
-    "rat",
-    "reindeer",
-    "reptile",
-    "rhinoceros",
-    "salamander",
-    "seal",
-    "sheep",
-    "shrew",
-    "silver fox",
-    "skunk",
-    "sloth",
-    "snake",
-    "squirrel",
-    "tapir",
-    "tiger",
-    "toad",
-    "turtle",
-    "walrus",
-    "warthog",
-    "weasel",
-    "whale",
-    "wildcat",
-    "wolf",
-    "wolverine",
-    "wombat",
-    "woodchuck",
-    "yak",
-    "zebra"
+const commands = [
+    {
+        "command": /set the text to (.*)/,
+        "callback": setText
+    },
+    {
+        "command": /set the red value to (.*)/,
+        "callback": setRed
+    },
+    {
+        "command": /set the text color to (.*) gray/,
+        "callback": setTextColor
+    },
+    {
+        "command": /set the background to (\d+) (\d+) (\d+)/,
+        "callback": setBackground
+    }
 ];
 
-const QUESTION_DELAY = 2000; // in milliseconds
+const voiceRecognizer = new p5.SpeechRec();
 
-// The current answer to display (we use it initially to display the click instruction)
-let currentAnswer = `Click to begin.`;
-// The current animal name the user is trying to guess
-let currentAnimal = ``;
+let displayText = `...`;
+let bgColor = {
+    r: 0,
+    g: 0,
+    b: 0
+};
+let textColor = 255;
 
-// The speech synthesizer
-const speechSynthesizer = new p5.Speech();
-// The speech recognizer
-const speechRecognizer = new p5.SpeechRec();
-
-/**
-Create a canvas
-Set up annyang with the guessing command
-Set text defaults
-*/
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 400);
 
-    // Set up the recognizer
-    // Make it listen continuously. Not ideal but necessary.
-    speechRecognizer.continuous = true;
-    // Tell it the function to call on a result
-    speechRecognizer.onResult = handleVoiceInput;
-    // Start it
-    speechRecognizer.start();
-
-    // Text defaults
-    textSize(102);
-    textStyle(BOLD);
-    textAlign(CENTER);
+    voiceRecognizer.continuous = true;
+    voiceRecognizer.onResult = handleCommand;
+    voiceRecognizer.start();
 }
 
-
-/**
-Display the current answer.
- */
 function draw() {
-    background(0);
+    background(bgColor.r, bgColor.g, bgColor.b);
 
-    displayAnswer();
+    push()
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    fill(textColor);
+    text(displayText, width / 2, height / 2);
+    pop();
 }
 
-/**
-Display the current answer in red if incorrect and green if correct
-(Displays nothing if no guess entered yet)
-*/
-function displayAnswer() {
-    if (currentAnswer === currentAnimal) {
-        fill(0, 255, 0);
+function handleCommand() {
+    if (!voiceRecognizer.resultValue) {
+        return;
     }
-    else {
-        fill(255, 0, 0);
-    }
-    text(currentAnswer, width / 2, height / 2);
-}
 
-/**
-Reverse the animal name and say it with the synthesizer
-*/
-function sayAnimalBackwards(animal) {
-    let reverseAnimal = reverseString(animal);
-    speechSynthesizer.speak(reverseAnimal);
-}
-
-/**
-Reverses the provided string
-*/
-function reverseString(string) {
-    // Split the string into an array of characters
-    let characters = string.split('');
-    // Reverse the array of characters
-    let reverseCharacters = characters.reverse();
-    // Join the array of characters back into a string
-    let result = reverseCharacters.join('');
-    // Return the result
-    return result;
-}
-
-/**
-Called by the recognizer when the user make a guess.
-Sets the answer text to the guess.
-*/
-function handleVoiceInput() {
-    // Set a default that works if we don't get a useful input
-    let guessedAnimal = `what??`;
-    // Make sure there is a result
-    if (speechRecognizer.resultValue) {
-        // We're going to use split() to break what the user said into two parts
-        // The part *before* they say "I think it is" and the part *after* they say it
-        // The *after* part should be their guessed animal...
-        let parts = speechRecognizer.resultString.toLowerCase().split(`i think it is `);
-        if (parts.length > 1) {
-            guessedAnimal = parts[1];
+    for (let command of commands) {
+        let lowercase = voiceRecognizer.resultString.toLowerCase();
+        let match = lowercase.match(command.command);
+        console.log(match);
+        if (match && match.length > 1) {
+            command.callback(match);
         }
     }
-    // Convert the guess to lowercase to match the answer format
-    currentAnswer = guessedAnimal;
 }
 
-/**
-Reset the answer text, get a new random animal, say its name
-*/
-function nextQuestion() {
-    currentAnswer = ``;
-    currentAnimal = random(animals);
-    sayAnimalBackwards(currentAnimal);
+function setText(data) {
+    displayText = data[1];
 }
 
-/**
-When the user clicks, go to the next question
-*/
-function mousePressed() {
-    nextQuestion();
+function setRed(data) {
+    bgColor.r = parseInt(data[1]);
 }
 
-// let currentAnimal = ``;
-// let currentAnswer = ``;
+function setTextColor(data) {
+    textColor = parseInt(data[1]);
+}
 
-// const speechSynthesizer = new p5.Speech();
-// const speechRecognizer = new p5.SpeechRec();
+function setBackground(data) {
+    if (data.length > 3) {
+        bgColor.r = parseInt(data[1]);
+        bgColor.g = parseInt(data[2]);
+        bgColor.b = parseInt(data[3]);
+    }
+}
 
 // let state = `title`; // Can be: title, simulation
 
@@ -279,9 +101,6 @@ function mousePressed() {
 // function setup() {
 //     createCanvas(500, 500);
 
-//     speechRecognizer.continuous = true;
-//     speechRecognizer.onResult = handleSpeechInput;
-//     speechRecognizer.start();
 // }
 
 // // draw() displays all the different states and their functions
@@ -296,17 +115,6 @@ function mousePressed() {
 //         simulation();
 //     }
 
-//     if (currentAnswer === currentAnimal) {
-//         console.log(`Correct.`);
-//         fill(50, 200, 50);
-//     }
-//     else {
-//         console.log(`Incorrect.`);
-//         fill(200, 50, 50);
-//     }
-//     textSize(32);
-//     textAlign(CENTER, CENTER);
-//     text(currentAnswer, width / 2, height / 2);
 // }
 
 // function title() {
@@ -338,36 +146,6 @@ function mousePressed() {
 //     handleSpeechInput();
 //     mousePressed();
 //     reverseString(string);
-// }
-
-// function handleSpeechInput() {
-//     let guessedAnimal = `What???`;
-//     if (speechRecognizer.resultValue) {
-//         let lowerCaseResult = speechRecognizer.resultString.toLowerCase();
-//         let parts = lowerCaseResult.split(`i think it is `);
-//         if (parts.length > 1) {
-//             guessedAnimal = parts[1];
-//         }
-//     }
-//     currentAnswer = guessedAnimal;
-//     console.log(currentAnswer);
-// }
-
-// function mousePressed() {
-//     currentAnimal = random(animals);
-//     let reverseAnimal = reverseString(currentAnimal);
-//     speechSynthesizer.speak(reverseAnimal);
-// }
-
-// function reverseString(string) {
-//     // Split the string into an array of characters
-//     let characters = string.split(``);
-//     // Reverse the array of characters
-//     let reverseCharacters = characters.reverse();
-//     // Join the array of characters back into a string
-//     let result = reverseCharacters.join(``);
-//     // Return the result
-//     return result;
 // }
 
 // // Calls the keyPressed function to work
