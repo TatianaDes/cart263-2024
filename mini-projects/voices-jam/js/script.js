267a6aff522247cc9a6838a443786711ecc04190
TatianaDes <tatiana.d0714@gmail.com>
2024-01-30 01:11:52 -0500

    MP1: Added slamina to my program to try out different things, string function not working

--- mini-projects/voices-jam/js/script.js
+++ mini-projects/voices-jam/js/script.js
@@ -7,21 +7,166 @@ This is a portotype of a program about the importance of being kind towards all 
 
 "use strict";
 
+
+const animals = [
+    "aardvark",
+    "alligator",
+    "alpaca",
+    "antelope",
+    "ape",
+    "armadillo",
+    "baboon",
+    "badger",
+    "bat",
+    "bear",
+    "beaver",
+    "bison",
+    "boar",
+    "buffalo",
+    "bull",
+    "camel",
+    "canary",
+    "capybara",
+    "cat",
+    "chameleon",
+    "cheetah",
+    "chimpanzee",
+    "chinchilla",
+    "chipmunk",
+    "cougar",
+    "cow",
+    "coyote",
+    "crocodile",
+    "crow",
+    "deer",
+    "dingo",
+    "dog",
+    "donkey",
+    "dromedary",
+    "elephant",
+    "elk",
+    "ewe",
+    "ferret",
+    "finch",
+    "fish",
+    "fox",
+    "frog",
+    "gazelle",
+    "gila monster",
+    "giraffe",
+    "gnu",
+    "goat",
+    "gopher",
+    "gorilla",
+    "grizzly bear",
+    "ground hog",
+    "guinea pig",
+    "hamster",
+    "hedgehog",
+    "hippopotamus",
+    "hog",
+    "horse",
+    "hyena",
+    "ibex",
+    "iguana",
+    "impala",
+    "jackal",
+    "jaguar",
+    "kangaroo",
+    "koala",
+    "lamb",
+    "lemur",
+    "leopard",
+    "lion",
+    "lizard",
+    "llama",
+    "lynx",
+    "mandrill",
+    "marmoset",
+    "mink",
+    "mole",
+    "mongoose",
+    "monkey",
+    "moose",
+    "mountain goat",
+    "mouse",
+    "mule",
+    "muskrat",
+    "mustang",
+    "mynah bird",
+    "newt",
+    "ocelot",
+    "opossum",
+    "orangutan",
+    "oryx",
+    "otter",
+    "ox",
+    "panda",
+    "panther",
+    "parakeet",
+    "parrot",
+    "pig",
+    "platypus",
+    "polar bear",
+    "porcupine",
+    "porpoise",
+    "prairie dog",
+    "puma",
+    "rabbit",
+    "raccoon",
+    "ram",
+    "rat",
+    "reindeer",
+    "reptile",
+    "rhinoceros",
+    "salamander",
+    "seal",
+    "sheep",
+    "shrew",
+    "silver fox",
+    "skunk",
+    "sloth",
+    "snake",
+    "squirrel",
+    "tapir",
+    "tiger",
+    "toad",
+    "turtle",
+    "walrus",
+    "warthog",
+    "weasel",
+    "whale",
+    "wildcat",
+    "wolf",
+    "wolverine",
+    "wombat",
+    "woodchuck",
+    "yak",
+    "zebra"
+];
+let currentAnimal = ``;
+let currentAnswer = ``;
+
+const speechSynthesizer = new p5.Speech();
+const speechRecognizer = new p5.SpeechRec();
+
 let state = `title`; // Can be: title, simulation
 
-/**
- * Description of preload
-*/
+// preload() creates the images I wish to put in my program
 function preload() {
 
 }
 
+// setup() creates the canvas and the new classes
 function setup() {
     createCanvas(500, 500);
 
+    speechRecognizer.continuous = true;
+    speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.start();
 }
 
-
+// draw() displays all the different states and their functions
 function draw() {
     background(253, 222, 247);
 
@@ -32,6 +177,18 @@ function draw() {
     else if (state === `simulation`) {
         simulation();
     }
+
+    if (currentAnswer === currentAnimal) {
+        console.log(`Correct.`);
+        fill(50, 200, 50);
+    }
+    else {
+        console.log(`Incorrect.`);
+        fill(200, 50, 50);
+    }
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentAnswer, width / 2, height / 2);
 }
 
 function title() {
@@ -53,13 +210,46 @@ function title() {
     push();
     textSize(15);
     fill(197, 62, 93);
-    text(`Please use your microphone to speak to the computer`, 120, 470);
+    text(`Please use your microphone to speak to the computer`, 300, 470);
     pop();
 }
 
+
 function simulation() {
     // Simulation state
+    handleSpeechInput();
+    mousePressed();
+    reverseString(string);
+}
 
+function handleSpeechInput() {
+    let guessedAnimal = `What???`;
+    if (speechRecognizer.resultValue) {
+        let lowerCaseResult = speechRecognizer.resultString.toLowerCase();
+        let parts = lowerCaseResult.split(`i think it is `);
+        if (parts.length > 1) {
+            guessedAnimal = parts[1];
+        }
+    }
+    currentAnswer = guessedAnimal;
+    console.log(currentAnswer);
+}
+
+function mousePressed() {
+    currentAnimal = random(animals);
+    let reverseAnimal = reverseString(currentAnimal);
+    speechSynthesizer.speak(reverseAnimal);
+}
+
+function reverseString(string) {
+    // Split the string into an array of characters
+    let characters = string.split(``);
+    // Reverse the array of characters
+    let reverseCharacters = characters.reverse();
+    // Join the array of characters back into a string
+    let result = reverseCharacters.join(``);
+    // Return the result
+    return result;
 }
 
 // Calls the keyPressed function to work
