7bdbf7e1e30e96b07e2bb80b906bc68aaa54b916
TatianaDes <tatiana.d0714@gmail.com>
2024-01-31 18:47:11 -0500

    MP1: Changed the names of some of the functions and changed the responses to the prompts. Not working except one.

--- mini-projects/voices-jam/js/script.js
+++ mini-projects/voices-jam/js/script.js
@@ -12,19 +12,19 @@ let state = `title`; // Can be: title, simulation
 const commands = [
     {
         "command": /i feel lost (.*)/,
-        "callback": setText
+        "callback": setLost
     },
     {
         "command": /i need words of wisdom (.*)/,
-        "callback": setRed
+        "callback": setWisdom
     },
     {
-        "command": /i keep procrasting my work (.*)/,
-        "callback": setTextColor
+        "command": /i keep procrastinating my work (.*)/,
+        "callback": setProcrastinating
     },
     {
         "command": /how do i know i can trust you (.*)/,
-        "callback": setBackground
+        "callback": setTrust
     }
 ];
 
@@ -32,8 +32,9 @@ const speechSynthesizer = new p5.Speech();
 const voiceRecognizer = new p5.SpeechRec();
 
 let showSubtitle = false;
+
 // kindComp();
-let toSay = `i'm crawling through your air conditioning ducts right now.`;
+let toSay = `when you feel lost, you can always remember that there are good people in your life willing to help you.`;
 // upsetComp();
 let toSay2 = `i will find you.`;
 
@@ -43,14 +44,14 @@ let toSay3 = `sometimes lessons need to be learned through tough experiences, bu
 let toSay4 = `the only advice i have for you is that you should watch your back.`;
 
 // happyComp();
-let toSay5 = `i'm crawling through your air conditioning ducts right now.`;
+let toSay5 = `have you thought about making yourself a schedule? give yourself periods of time when you are working and when you are taking breaks.`;
 // angryComp();
-let toSay6 = `i will find you.`;
+let toSay6 = `if you keep procrastinating your work, i will shut off this computer and you will not want to know what is behind your door.`;
 
 // goodComp();
-let toSay7 = `sometimes lessons need to be learned through tough experiences, but you will get through this.`;
+let toSay7 = `because i am just a computer spitting back all the information humans have already put into me. so you have nothing to fear, think of me as a human being but with a lot quicker capabilities.`;
 // evilComp();
-let toSay8 = `the only advice i have for you is that you should watch your back.`;
+let toSay8 = `you are right, maybe you should not trust me, how do you know i am not in your house right now. you hear that rustling outside your door, how do you know it is not me?`;
 
 let displayText = `...`;
 let bgColor = {
@@ -147,7 +148,7 @@ function handleCommand() {
     }
 }
 
-function setText(data) {
+function setLost(data) {
     if (data[1] === "please help me." || data[1] === "help me please.") {
         kindComp();
     }
@@ -173,7 +174,7 @@ function upsetComp() {
     speechSynthesizer.speak(toSay2);
 }
 
-function setRed(data) {
+function setWisdom(data) {
     if (data[1] === "please let me know what i can do." || data[1] === "tell me what to do please.") {
         niceComp();
     }
@@ -185,16 +186,23 @@ function setRed(data) {
 function niceComp() {
     // computer answers of course I will help you!
     // Say something!
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice(`Microsoft Linda - English (Canada)`);
     speechSynthesizer.speak(toSay3);
 }
 function rudeComp() {
     // computer doesn't help
+    // Synthesis settings
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice(`Microsoft Linda - English (Canada)`);
     speechSynthesizer.speak(toSay4);
 }
 
 
 
-function setTextColor(data) {
+function setProcrastinating(data) {
     if (data[1] === "please give me advice on how to get motivated." || data[1] === "motivate me please.") {
         happyComp();
     }
@@ -206,14 +214,20 @@ function setTextColor(data) {
 function happyComp() {
     // computer answers of course I will help you!
     // Say something!
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice(`Microsoft Linda - English (Canada)`);
     speechSynthesizer.speak(toSay5);
 }
 function angryComp() {
     // computer doesn't help
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice(`Microsoft Linda - English (Canada)`);
     speechSynthesizer.speak(toSay6);
 }
 
-function setBackground(data) {
+function setTrust(data) {
     if (data[1] === "please prove it to me" || data[1] === "prove it to me please") {
         goodComp();
     }
@@ -225,10 +239,16 @@ function setBackground(data) {
 function goodComp() {
     // computer answers of course I will help you!
     // Say something!
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice(`Microsoft Linda - English (Canada)`);
     speechSynthesizer.speak(toSay7);
 }
 function evilComp() {
     // computer doesn't help
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice(`Microsoft Linda - English (Canada)`);
     speechSynthesizer.speak(toSay8);
 }
 
@@ -239,68 +259,3 @@ function speechStarted() {
 function speechEnded() {
     showSubtitle = false;
 }
-
-// let state = `title`; // Can be: title, simulation
-
-// // preload() creates the images I wish to put in my program
-// function preload() {
-
-// }
-
-// // setup() creates the canvas and the new classes
-// function setup() {
-//     createCanvas(500, 500);
-
-// }
-
-// // draw() displays all the different states and their functions
-// function draw() {
-//     background(253, 222, 247);
-
-//     // Setting up all the different states
-//     if (state === `title`) {
-//         title();
-//     }
-//     else if (state === `simulation`) {
-//         simulation();
-//     }
-
-// }
-
-// function title() {
-//     // Title state
-//     push();
-//     textSize(50);
-//     fill(247, 130, 189);
-//     textAlign(CENTER, CENTER);
-//     text(`Say Please`, width / 2, height / 2);
-//     pop();
-
-//     push();
-//     textSize(17);
-//     fill(206, 90, 130);
-//     textAlign(CENTER, CENTER);
-//     text(`(Please press the Space Bar to Start)`, width / 2, 300);
-//     pop();
-
-//     push();
-//     textSize(15);
-//     fill(197, 62, 93);
-//     text(`Please use your microphone to speak to the computer`, 300, 470);
-//     pop();
-// }
-
-
-// function simulation() {
-//     // Simulation state
-//     handleSpeechInput();
-//     mousePressed();
-//     reverseString(string);
-// }
-
-// // Calls the keyPressed function to work
-// function keyPressed() {
-//     if (state === `title`) {
-//         state = `simulation`;
-//     }
-// }
