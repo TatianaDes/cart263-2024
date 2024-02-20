f38bc5dda449f9e7e8b4f0b10592ca645c4c2b17
TatianaDes <tatiana.d0714@gmail.com>
2024-02-20 15:17:08 -0500

    MP2: Created a parameter for all of the fingers and created new functions to contain them

--- mini-projects/ai-jam/js/script.js
+++ mini-projects/ai-jam/js/script.js
@@ -27,6 +27,9 @@ let loveSize = 3;
 let priority = [];
 let prioritySize = 2;
 
+let tipX = [];
+let tipY = [];
+
 // setup() creates the canvas and the calling of the handpose program as well as the webcam. It also gives the arrays random positions on the canvas
 function setup() {
     createCanvas(640, 480);
@@ -42,7 +45,7 @@ function setup() {
 
     // Listen for predictions
     handpose.on(`predict`, function (results) {
-        console.log(results);
+        // console.log(results);
         predictions = results;
     });
 
@@ -130,13 +133,14 @@ function simulation() {
 
     // Calling the functions for all the hearts
     for (let j = 0; j < love.length; j++) {
-        // checkOverlap(love[j]);
+        checkOverlap(love[j]);
+        moveHeart(love[j]);
         displayHeart(love[j]);
     }
 
     // Calling the functions for all the plans
     for (let j = 0; j < priority.length; j++) {
-        // movePlans(priority[j]);
+        movePlans(priority[j]);
         displayPlans(priority[j]);
     }
 }
@@ -145,140 +149,84 @@ function simulation() {
 function prepareHand() {
     if (predictions.length > 0) {
         let hand = predictions[0];
-        // Creating the thumb recognition
-        let thumb = hand.annotations.thumb;
-        let tipT = thumb[3];
-        let baseT = thumb[2];
-        let tipTX = tipT[0];
-        let tipTY = tipT[1];
-        let baseTX = baseT[0];
-        let baseTY = baseT[1];
-
-        // Creating the index finger recognition
-        let index = hand.annotations.indexFinger;
-        let tipIF = index[3];
-        let baseIF = index[2];
-        let tipIFX = tipIF[0];
-        let tipIFY = tipIF[1];
-        let baseIFX = baseIF[0];
-        let baseIFY = baseIF[1];
-
-        // Creating the middle finger recognition
-        let middle = hand.annotations.middleFinger;
-        let tipMF = middle[3];
-        let baseMF = middle[2];
-        let tipMFX = tipMF[0];
-        let tipMFY = tipMF[1];
-        let baseMFX = baseMF[0];
-        let baseMFY = baseMF[1];
-
-        // Creating the ring finger recognition
-        let ring = hand.annotations.ringFinger;
-        let tipRF = ring[3];
-        let baseRF = ring[2];
-        let tipRFX = tipRF[0];
-        let tipRFY = tipRF[1];
-        let baseRFX = baseRF[0];
-        let baseRFY = baseRF[1];
-
-        // Creating the pinky finger recognition
-        let pinky = hand.annotations.pinky;
-        let tipPF = pinky[3];
-        let basePF = pinky[2];
-        let tipPFX = tipPF[0];
-        let tipPFY = tipPF[1];
-        let basePFX = basePF[0];
-        let basePFY = basePF[1];
-
-        // Displaying the Thumb
-        push();
-        noFill();
-        stroke(77, 66, 95);
-        strokeWeight(12);
-        line(baseTX, baseTY, tipTX, tipTY);
-        pop();
-
-        // Displaying the Index Finger
-        push();
-        noFill();
-        stroke(125, 121, 158);
-        strokeWeight(7);
-        line(baseIFX, baseIFY, tipIFX, tipIFY);
-        pop();
-
-        // Displaying the Middle Finger
-        push();
-        noFill();
-        stroke(102, 69, 108);
-        strokeWeight(10);
-        line(baseMFX, baseMFY, tipMFX, tipMFY);
-        pop();
-
-        // Displaying the Ring Finger
-        push();
-        noFill();
-        stroke(101, 98, 131);
-        strokeWeight(8);
-        line(baseRFX, baseRFY, tipRFX, tipRFY);
-        pop();
-
-        // Displaying the Pinky Finger
-        push();
-        noFill();
-        stroke(125, 98, 131);
-        strokeWeight(5);
-        line(basePFX, basePFY, tipPFX, tipPFY);
-        pop();
+        drawHand(hand.annotations.thumb, 77, 66, 95, 12);
+        drawHand(hand.annotations.indexFinger, 125, 121, 158, 7);
+        drawHand(hand.annotations.middleFinger, 102, 69, 108, 10);
+        drawHand(hand.annotations.ringFinger, 101, 98, 131, 8);
+        drawHand(hand.annotations.pinky, 125, 98, 131, 5);
     }
 }
 
-// // Checks the overlaps of the middle fingers tip and whatever it is touching
-// function checkOverlap(heart) {
-//     // Check if middle finger and the heart overlap
-//     let d = dist(tipMFX, tipMFY, heart.x, heart.y);
-//     if (d < heart.size / 2) {
-//         heart.vx = heart.scaredSpeed;
-//     }
+function drawHand(finger, strokeR, strokeG, strokeB, strokeW) {
+  // Creating the middle finger recognition
+  let myFinger = finger;
+  let tipF = myFinger[3];
+  let baseF = myFinger[2];
+  let tipFX = tipF[0];
+  let tipFY = tipF[1];
+  let baseFX = baseF[0];
+  let baseFY = baseF[1];
 
-//     // Move the hearts
-// heart.x += heart.vx;
-// heart.y += heart.vy;
-// }
+    // Displaying the Middle Finger
+    push();
+    noFill();
+    stroke(strokeR, strokeG, strokeB);
+    strokeWeight(strokeW);
+    line(baseFX, baseFY, tipFX, tipFY);
+    pop();
 
-// // Make the plans run away from the middle finger tip
-// function movePlans(plans) {
-//     // Make the puppies scared of the dog
-//     let a = dist(tipMFX, tipMFY, plans.x, plans.y);
-//     if (a < tipMFX / 2 + plans.size / 2 + 300) {
-//         if (tipMFX < plans.x) {
-//             plans.ax = plans.acceleration;
-//         }
-//         else {
-//             plans.ax = -plans.acceleration;
-//         }
+    tipX.push(tipFX);
+    tipY.push(tipFY);
+}
 
-//         if (tipMFY < plans.y) {
-//             plans.ay = plans.acceleration;
-//         }
-//         else {
-//             plans.ay = -plans.acceleration;
-//         }
-//     }
+// Checks the overlaps of the middle fingers tip and whatever it is touching
+function checkOverlap(heart) {
+    // Check if middle finger and the heart overlap
+for (let j = 0; j < tipX.length; j++) {
+    let d = dist(tipX[j], tipY[j], heart.x, heart.y);
+    if (d < heart.size * 3) {
+        heart.vx = heart.scaredSpeed;
+    }
+}
+}
 
-//     // Constraining the speed and movement of the plans from their x-axis and y-axis
-//     plans.vx = plans.vx + plans.ax;
-//     plans.vx = constrain(plans.vx, -plans.maxSpeed, plans.maxSpeed);
-//     plans.vy = plans.vy + plans.ay;
-//     plans.vy = constrain(plans.vy, -plans.maxSpeed, plans.maxSpeed);
+function moveHeart(heart) {
+// Move the hearts
+heart.x += heart.vx;
+heart.y += heart.vy;
+}
 
-//     // Position is being added onto the velocity of plans
-//     plans.x = plans.x + plans.vx;
-//     plans.y = plans.y + plans.vy;
+// Make the plans run away from the middle finger tip
+function movePlans(plans) {
+    // Make the puppies scared of the dog
+    for (let j = 0; j < tipX.length; j++) {
+    let a = dist(tipX[j], tipY[j], plans.x, plans.y);
+    if (a < tipX[j] / 2 + plans.size / 2 + 300) {
+        if (tipX[j] < plans.x) {
+            plans.ax = plans.acceleration;
+        }
+        else {
+            plans.ax = -plans.acceleration;
+        }
 
-//     // Constrain the plans to the width and height of the canvas
-//     plans.x = constrain(plans.x, 0, width);
-//     plans.y = constrain(plans.y, 0, height);
-// }
+        if (tipY[j] < plans.y) {
+            plans.ay = plans.acceleration;
+        }
+        else {
+            plans.ay = -plans.acceleration;
+        }
+    }
+}
+    // Constraining the speed and movement of the plans from their x-axis and y-axis
+ plans.vx = plans.vx + plans.ax;
+ plans.vx = constrain(plans.vx, -plans.maxSpeed, plans.maxSpeed);
+ plans.vy = plans.vy + plans.ay;
+ plans.vy = constrain(plans.vy, -plans.maxSpeed, plans.maxSpeed);
+
+ // Position is being added onto the velocity of plans
+ plans.x = plans.x + plans.vx;
+ plans.y = plans.y + plans.vy;
+}
 
 // Display the hearts
 function displayHeart(heart) {
