55667d4b4f113f154f2e8a00e7fe713ef41110ac
TatianaDes <tatiana.d0714@gmail.com>
2024-02-23 13:47:58 -0500

    MP2: Trying to make it so that the hearts only react to the middle finger (not working)

--- mini-projects/ai-jam/js/script.js
+++ mini-projects/ai-jam/js/script.js
@@ -198,9 +198,11 @@ function drawHand(finger, strokeR, strokeG, strokeB, strokeW) {
 function checkCloseness(heart) {
     // Check if fingers and the heart get close to each other
     for (let j = 0; j < tipX.length; j++) {
-        let d = dist(tipX[j], tipY[j], heart.x, heart.y);
-        if (d < heart.size * 3) {
-            heart.vx = heart.scaredSpeed;
+        if (j !== 0 && j !== 1 && j !== 3 && j !== 4) {
+            let d = dist(tipX[j], tipY[j], heart.x, heart.y);
+            if (d < heart.size * 3) {
+                heart.vx = heart.scaredSpeed;
+            }
         }
     }
 }
