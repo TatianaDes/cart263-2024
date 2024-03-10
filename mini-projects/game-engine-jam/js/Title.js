06c93e8bdc235c011f4fb2d187c60adda9338504
TatianaDes <tatiana.d0714@gmail.com>
2024-03-10 00:31:33 -0500

    MP3: Added a start sprite and made it so that when the cursor hovers over it it changes tint but it has no activity at the moment

--- mini-projects/game-engine-jam/js/Title.js
+++ mini-projects/game-engine-jam/js/Title.js
@@ -6,14 +6,41 @@ class Title extends Phaser.Scene {
         });
     }
 
+    // Loads all the images I would like to use.
+    preload() {
+        this.load.image(`start`, `assets/images/start.png`);
+    }
+
     // Creates the background colour and all the assets I would like to display on this scene.
     create() {
         // Creates background colour.
         this.cameras.main.setBackgroundColor(`#7a3131`);
 
+        // Creates the button sprite .
+        const bg = this.add.image(0, 0, 'start');
+
+        // Makes a conatiner around it that allows for it to change colour when the cursor hovers over it.
+        const container = this.add.container(400, 360, [bg]);
+
+        container.setSize(bg.width, bg.height);
+
+        container.setInteractive();
+
+        container.on('pointerover', () => {
+
+            bg.setTint(0x44ff44);
+
+        });
+
+        container.on('pointerout', () => {
+
+            bg.clearTint();
+
+        });
+
         // Creates text with all their different sizes and colours.
         this.add.text(150, 250, `Hard to Move On`, { fontFamily: `Lora`, fontSize: 64, color: `#ff7171` });
-        this.add.text(220, 320, `(Press the Spacebar Key to Continue)`, { fontFamily: `Lora`, fontSize: 20, color: `#000000` });
+        // this.add.text(220, 320, `(Press the Spacebar Key to Continue)`, { fontFamily: `Lora`, fontSize: 20, color: `#000000` });
         this.add.text(550, 550, `Move around with the arrow keys`, { fontFamily: `Lora`, fontSize: 15, color: `#9e7c7c` });
 
         // Allows the spacebar to call the next scene to start the program.
