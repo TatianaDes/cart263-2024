c2bb8c53fba4ff9853f11b601b724a0edb74ef5a
TatianaDes <tatiana.d0714@gmail.com>
2024-03-28 10:29:20 -0400

    FPP: Added text and colour to the forNothing scene

--- projects/final-project-prototype/js/ForNothing.js
+++ projects/final-project-prototype/js/ForNothing.js
@@ -9,13 +9,13 @@ class ForNothing extends Phaser.Scene {
     // Creates the background colour and all the assets I would like to display on this scene.
     create() {
         // Creates background colour.
-        this.cameras.main.setBackgroundColor(`#033d0c`);
+        this.cameras.main.setBackgroundColor(`#979797`);
 
         // Creates the button sprite .
         const bg = this.add.image(0, 0, 'continueG');
 
         // Creates text with all their different sizes and colours.
-        this.add.text(250, 250, `The Bargaining Level`, { fontFamily: `Lora`, fontSize: 35, color: `#a2c2a0` });
+        this.add.text(100, 150, `Was this really all for nothing?\nWere my efforts pointless in the end?\nI tried.\nI tried and I tried and I tried, and what did I get from all of this.\nNothing.\nYou could not even be honest with me by the end,\nand you struggled so much to prioritize me,\nlet alone the way you acted like a child when it was all over.\nWhat was the point in trying for you any longer?\n`, { fontFamily: `Lora`, fontSize: 25, color: `#cbcd21` });
 
         // Makes a conatiner around it that allows for it to change colour when the cursor hovers over it.
         const container = this.add.container(400, 530, [bg]);
