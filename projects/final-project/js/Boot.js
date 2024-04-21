fabf946f5d366b98e3afa16bece25d605da03ab0
TatianaDes <tatiana.d0714@gmail.com>
2024-04-21 00:11:03 -0400

    FP: Changing the boot layout so everything is organized in its own function

--- projects/final-project/js/Boot.js
+++ projects/final-project/js/Boot.js
@@ -9,12 +9,42 @@ class Boot extends Phaser.Scene {
     // Loads all the images I would like to use.
     preload() {
 
+        this.soundPreload();
+
+        this.mainCharactersPreload();
+
+        this.denialPreload();
+
+        this.angerPreload();
+
+        this.bargainingPreload();
+
+        this.depressionPreload();
+
+        this.acceptancePreload();
+
+        this.buttonPreload();
+
+        this.changeScenePreload();
+    }
+
+    // Creates a function that allows all code that wants to be done immediately on the program.
+    create() {
+        // Calls the createAnimation() function.
+        this.createAnimations();
+    }
+
+    // Creates changes for individual frames so that each frame could have its own event.
+    update() {
+
+    }
+
+    soundPreload() {
         this.load.audio("coyoteGrowl", ['assets/sounds/coyotegrowl.mp3']);
         this.load.audio("sheepHerd", ['assets/sounds/sheepherd.mp3']);
+    }
 
-        // Loads the tree in the Denial scene.
-        this.load.image('tree', 'assets/images/tree.png');
-
+    mainCharactersPreload() {
         // Loads the sheep sprite.
         this.load.spritesheet('sheep', 'assets/images/sheep.png', {
             frameWidth: 38,
@@ -33,6 +63,11 @@ class Boot extends Phaser.Scene {
             frameHeight: 36,
             endFrame: 7,
         });
+    }
+
+    denialPreload() {
+        // Loads the tree in the Denial scene.
+        this.load.image('tree', 'assets/images/tree.png');
 
         // Loads the flower in the Denial scene.
         this.load.spritesheet('flower', 'assets/images/flower.png', {
@@ -40,8 +75,9 @@ class Boot extends Phaser.Scene {
             frameHeight: 30,
             endFrame: 4,
         });
+    }
 
-
+    angerPreload() {
         // Creating objects for the Anger scene.
         this.load.image('jar', 'assets/images/jar.png');
         this.load.image('scale', 'assets/images/scale.png');
@@ -51,7 +87,9 @@ class Boot extends Phaser.Scene {
             frameHeight: 66,
             endFrame: 1,
         });
+    }
 
+    bargainingPreload() {
         // Creating objects for the Bargaining scene.
         this.load.image('beer', 'assets/images/beer.png');
         this.load.image('chips', 'assets/images/chips.png');
@@ -60,7 +98,9 @@ class Boot extends Phaser.Scene {
             frameHeight: 171,
             endFrame: 4,
         });
+    }
 
+    depressionPreload() {
         // Creating objects for the Depression scene.
         this.load.image('bed', 'assets/images/bed.png');
         this.load.image('rain', 'assets/images/rain.png');
@@ -70,7 +110,9 @@ class Boot extends Phaser.Scene {
             frameHeight: 80,
             endFrame: 4,
         });
+    }
 
+    acceptancePreload() {
         // Creating objects for the Acceptance scene.
         // Loads the sheep sprite.
         this.load.spritesheet('friends', 'assets/images/friends.png', {
@@ -78,7 +120,9 @@ class Boot extends Phaser.Scene {
             frameHeight: 30,
             endFrame: 7,
         });
+    }
 
+    buttonPreload() {
         // Loads the start and start over buttons for the title and ending scenes.
         this.load.image('start', 'assets/images/start.png');
         this.load.image('start-overR', 'assets/images/start-overR.png');
@@ -86,23 +130,13 @@ class Boot extends Phaser.Scene {
         // Creating continue buttons instead of start over buttons.
         this.load.image('continueG', 'assets/images/continueG.png');
         this.load.image('continueB', 'assets/images/continueB.png');
+    }
 
-
+    changeScenePreload() {
         // Calls on the next scene keyname to occur automatically once the program boots up.
         this.load.on('complete', () => {
             this.scene.start('title');
         });
-    }
-
-    // Creates a function that allows all code that wants to be done immediately on the program.
-    create() {
-        // Calls the createAnimation() function.
-        this.createAnimations();
-    }
-
-    // Creates changes for individual frames so that each frame could have its own event.
-    update() {
-
     }
 
     // Creates the animations for what frames are used of the sprite when it is in movement and when it is idle.
